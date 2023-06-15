41f790d77315173f22456a4cad629a691a112184
Giselle Barbosa Ferraro <giselle.barbosa.ferraro@gmail.com>
2023-06-14 21:21:01 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    //document.querySelector('header button');
+    //console.log($('#botao-cancelar'));
+    //document.querySelector('header button').addEventListener('click',function(e){  
+    //})
+//Esses códigos podem ser escritos como os de baixo:
+
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
+    })
+})
+
